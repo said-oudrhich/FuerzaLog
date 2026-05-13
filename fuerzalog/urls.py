1ea8d6b0dfbd878a50a741a4e08781c208ecd9d9
Said Oudrhich <said.oudrhich@educa.madrid.org>
2026-05-13 18:06:34 +0200

    configuracion basica lista

---
+++ fuerzalog/urls.py
@@ -0,0 +1,19 @@
+"""
+Rutas principales del proyecto FuerzaLog.
+
+Aquí se definen las URLs de administración, la aplicación principal y las
+rutas de autenticación de Django.
+"""
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('entrenamientos.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
