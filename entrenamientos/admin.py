f682e6d9845297c8ba09ee09626646af4fb76d9c
Said Oudrhich <said.oudrhich@educa.madrid.org>
2026-05-14 00:11:21 +0200

    actualizo admin y formularios para los nuevos modelos

--- entrenamientos/admin.py
+++ entrenamientos/admin.py
@@ -1,6 +1,48 @@
 from django.contrib import admin
-from .models import Entrenamiento, GrupoMuscular, Rutina
+from .models import GrupoMuscular, Ejercicio, Rutina, RutinaEjercicio, SerieRutina, Workout, WorkoutEjercicio, Serie
 
-admin.site.register(GrupoMuscular)
-admin.site.register(Rutina)
-admin.site.register(Entrenamiento)
+
+@admin.register(GrupoMuscular)
+class GrupoMuscularAdmin(admin.ModelAdmin):
+    list_display = ['nombre']
+    ordering = ['nombre']
+
+
+@admin.register(Ejercicio)
+class EjercicioAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'grupo_muscular', 'usuario']
+    list_filter = ['grupo_muscular']
+    search_fields = ['nombre']
+
+
+class SerieRutinaInline(admin.TabularInline):
+    model = SerieRutina
+    extra = 1
+
+
+class RutinaEjercicioInline(admin.TabularInline):
+    model = RutinaEjercicio
+    extra = 1
+
+
+@admin.register(Rutina)
+class RutinaAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'usuario', 'fecha_creacion']
+    inlines = [RutinaEjercicioInline]
+
+
+class SerieInline(admin.TabularInline):
+    model = Serie
+    extra = 0
+
+
+class WorkoutEjercicioInline(admin.TabularInline):
+    model = WorkoutEjercicio
+    extra = 0
+
+
+@admin.register(Workout)
+class WorkoutAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'fecha', 'usuario', 'rutina']
+    list_filter = ['usuario']
+    inlines = [WorkoutEjercicioInline]
--- entrenamientos/forms.py
+++ entrenamientos/forms.py
@@ -1,14 +1,70 @@
 from django import forms
-from .models import Entrenamiento, Rutina
+from django.forms import inlineformset_factory
+from django.db.models import Q
+from .models import Ejercicio, Rutina, RutinaEjercicio, SerieRutina, Workout, WorkoutEjercicio, Serie
 
 
-class EntrenamientoForm(forms.ModelForm):
+class EjercicioForm(forms.ModelForm):
     class Meta:
-        model = Entrenamiento
-        fields = ['titulo', 'descripcion', 'peso', 'series', 'repeticiones', 'imagen', 'grupo_muscular', 'rutina']
+        model = Ejercicio
+        fields = ['nombre', 'descripcion', 'grupo_muscular', 'imagen']
+        widgets = {
+            'descripcion': forms.Textarea(attrs={'rows': 3}),
+        }
 
 
 class RutinaForm(forms.ModelForm):
     class Meta:
         model = Rutina
         fields = ['nombre', 'descripcion']
+        widgets = {
+            'descripcion': forms.Textarea(attrs={'rows': 3}),
+        }
+
+
+class WorkoutForm(forms.ModelForm):
+    class Meta:
+        model = Workout
+        fields = ['titulo', 'fecha', 'duracion_min', 'rutina', 'notas']
+        widgets = {
+            'fecha': forms.DateInput(attrs={'type': 'date'}),
+            'notas': forms.Textarea(attrs={'rows': 3}),
+        }
+
+    def __init__(self, *args, usuario=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        if usuario:
+            self.fields['rutina'].queryset = Rutina.objects.filter(usuario=usuario)
+        self.fields['rutina'].required = False
+        self.fields['titulo'].required = False
+
+
+SerieFormSet = inlineformset_factory(
+    WorkoutEjercicio,
+    Serie,
+    fields=['tipo', 'peso_kg', 'repeticiones'],
+    extra=1,
+    can_delete=True,
+)
+
+
+class WorkoutEjercicioForm(forms.ModelForm):
+    class Meta:
+        model = WorkoutEjercicio
+        fields = ['ejercicio', 'notas']
+        widgets = {
+            'notas': forms.TextInput(attrs={'placeholder': 'Notas opcionales'}),
+        }
+
+    def __init__(self, *args, usuario=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        if usuario:
+            self.fields['ejercicio'].queryset = Ejercicio.objects.filter(
+                Q(usuario=usuario) | Q(usuario__isnull=True)
+            )
+
+
+class SerieForm(forms.ModelForm):
+    class Meta:
+        model = Serie
+        fields = ['tipo', 'peso_kg', 'repeticiones']
