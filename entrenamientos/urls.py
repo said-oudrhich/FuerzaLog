73f1445360705a0c6e3f62bbd1c1b07a02db14aa
Said Oudrhich <said.oudrhich@educa.madrid.org>
2026-05-13 18:06:47 +0200

    urls configuradas

---
+++ entrenamientos/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.EntrenamientoListView.as_view(), name='entrenamiento_list'),
+    path('entrenamiento/<int:pk>/', views.EntrenamientoDetailView.as_view(), name='entrenamiento_detail'),
+    path('entrenamiento/nuevo/', views.entrenamiento_create, name='entrenamiento_create'),
+    path('entrenamiento/<int:pk>/editar/', views.entrenamiento_update, name='entrenamiento_update'),
+    path('entrenamiento/<int:pk>/eliminar/', views.entrenamiento_delete, name='entrenamiento_delete'),
+    path('registro/', views.registro, name='registro'),
+    path('buscar/', views.buscar, name='buscar'),
+]
+
