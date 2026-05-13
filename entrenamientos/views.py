ba87f7a5dfd665933476814df75f435e5f96c154
Said Oudrhich <said.oudrhich@educa.madrid.org>
2026-05-13 18:06:46 +0200

    vistas listado y detalle

---
+++ entrenamientos/views.py
@@ -0,0 +1,110 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django import forms
+
+from .models import Entrenamiento
+
+
+class EntrenamientoListView(LoginRequiredMixin, ListView):
+    model = Entrenamiento
+    template_name = 'entrenamientos/lista_entrenamientos.html'
+    context_object_name = 'entrenamientos'
+
+    def get_queryset(self):
+        return Entrenamiento.objects.filter(usuario=self.request.user)
+
+
+class EntrenamientoDetailView(LoginRequiredMixin, DetailView):
+    model = Entrenamiento
+    template_name = 'entrenamientos/detalle_entrenamiento.html'
+
+
+@login_required
+def entrenamiento_create(request):
+    if request.method == 'POST':
+        form = EntrenamientoForm(request.POST, request.FILES)
+        if form.is_valid():
+            entrenamiento = form.save(commit=False)
+            entrenamiento.usuario = request.user
+            entrenamiento.save()
+            messages.success(request, 'Entrenamiento creado.')
+            return redirect('entrenamiento_list')
+    else:
+        form = EntrenamientoForm()
+    return render(request, 'entrenamientos/formulario_entrenamiento.html', {'form': form})
+
+
+@login_required
+def entrenamiento_update(request, pk):
+    entrenamiento = get_object_or_404(Entrenamiento, pk=pk)
+    if entrenamiento.usuario != request.user and not request.user.is_staff:
+        messages.error(request, 'No puedes editar esto.')
+        return redirect('entrenamiento_list')
+    if request.method == 'POST':
+        form = EntrenamientoForm(request.POST, request.FILES, instance=entrenamiento)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Entrenamiento actualizado.')
+            return redirect('entrenamiento_list')
+    else:
+        form = EntrenamientoForm(instance=entrenamiento)
+    return render(request, 'entrenamientos/formulario_entrenamiento.html', {'form': form})
+
+
+@login_required
+def entrenamiento_delete(request, pk):
+    entrenamiento = get_object_or_404(Entrenamiento, pk=pk)
+    if entrenamiento.usuario != request.user and not request.user.is_staff:
+        messages.error(request, 'No puedes borrar esto.')
+        return redirect('entrenamiento_list')
+    if request.method == 'POST':
+        entrenamiento.delete()
+        messages.success(request, 'Entrenamiento eliminado.')
+        return redirect('entrenamiento_list')
+    return render(request, 'entrenamientos/confirmar_eliminacion.html', {'entrenamiento': entrenamiento})
+
+
+def registro(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Cuenta creada correctamente.')
+            return redirect('entrenamiento_list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
+
+
+def inicio(request):
+    return render(request, 'inicio.html')
+
+
+from django.db.models import Q
+
+
+@login_required
+def buscar(request):
+    query = request.GET.get('q', '')
+    if query:
+        entrenamientos = Entrenamiento.objects.filter(
+            Q(titulo__icontains=query) | Q(descripcion__icontains=query),
+            usuario=request.user
+        )
+    else:
+        entrenamientos = Entrenamiento.objects.filter(usuario=request.user)
+    return render(request, 'entrenamientos/lista_entrenamientos.html', {'entrenamientos': entrenamientos})
+
+
+class EntrenamientoForm(forms.ModelForm):
+    class Meta:
+        model = Entrenamiento
+        fields = ['titulo', 'descripcion', 'peso', 'series', 'repeticiones', 'imagen', 'grupo_muscular', 'rutina']
+
