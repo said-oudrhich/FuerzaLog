2e3815b7bd0b23a15309ebe4e15a7d398fd799db
Said Oudrhich <said.oudrhich@educa.madrid.org>
2026-05-15 15:45:00 +0200

    corrijo puerto y allowed hosts para que funcione en Railway

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn fuerzalog.wsgi --log-file -
+web: gunicorn fuerzalog.wsgi --bind 0.0.0.0:$PORT --log-file -
--- fuerzalog/settings.py
+++ fuerzalog/settings.py
@@ -7,7 +7,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-uyngw&xm5alwx7^4uv2@+
 
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost 127.0.0.1').split()
+ALLOWED_HOSTS = os.environ.get(
+    'ALLOWED_HOSTS',
+    'localhost 127.0.0.1 fuerzalog.saiid.es .up.railway.app'
+).split()
 
 INSTALLED_APPS = [
     'django.contrib.admin',
