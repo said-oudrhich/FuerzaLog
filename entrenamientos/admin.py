e6fbfe911143b95fccf474e0c4e9fae983e64df5
Said Oudrhich <said.oudrhich@educa.madrid.org>
2026-05-19 15:15:11 +0200

    Deja admin con registro basico

--- entrenamientos/admin.py
+++ entrenamientos/admin.py
@@ -1,63 +1,12 @@
 from django.contrib import admin
-from .models import (
-    GrupoMuscular,
-    Ejercicio,
-    Rutina,
-    RutinaEjercicio,
-    SerieRutina,
-    Entrenamiento,
-    EntrenamientoEjercicio,
-    Serie,
-)
+from .models import GrupoMuscular, Ejercicio, Rutina, RutinaEjercicio, SerieRutina, Entrenamiento, EntrenamientoEjercicio, Serie
 
 
-admin.site.site_header = 'Administración de FuerzaLog'
-admin.site.site_title = 'FuerzaLog'
-admin.site.index_title = 'Panel de administración'
-
-
-@admin.register(GrupoMuscular)
-class GrupoMuscularAdmin(admin.ModelAdmin):
-    list_display = ['nombre']
-    search_fields = ['nombre']
-
-
-@admin.register(Ejercicio)
-class EjercicioAdmin(admin.ModelAdmin):
-    list_display = ['nombre', 'grupo_muscular', 'usuario']
-    list_filter = ['grupo_muscular']
-    search_fields = ['nombre']
-
-
-@admin.register(Rutina)
-class RutinaAdmin(admin.ModelAdmin):
-    list_display = ['nombre', 'usuario', 'fecha_creacion']
-    search_fields = ['nombre']
-
-
-@admin.register(RutinaEjercicio)
-class RutinaEjercicioAdmin(admin.ModelAdmin):
-    list_display = ['rutina', 'ejercicio', 'orden']
-
-
-@admin.register(SerieRutina)
-class SerieRutinaAdmin(admin.ModelAdmin):
-    list_display = ['rutina_ejercicio', 'num_series']
-
-
-@admin.register(Entrenamiento)
-class EntrenamientoAdmin(admin.ModelAdmin):
-    list_display = ['nombre', 'fecha', 'usuario', 'rutina']
-    list_filter = ['fecha']
-    search_fields = ['nombre']
-
-
-@admin.register(EntrenamientoEjercicio)
-class EntrenamientoEjercicioAdmin(admin.ModelAdmin):
-    list_display = ['entrenamiento', 'ejercicio', 'orden']
-
-
-@admin.register(Serie)
-class SerieAdmin(admin.ModelAdmin):
-    list_display = ['entrenamiento_ejercicio', 'tipo', 'peso_kg', 'repeticiones']
-    list_filter = ['tipo']
+admin.site.register(GrupoMuscular)
+admin.site.register(Ejercicio)
+admin.site.register(Rutina)
+admin.site.register(RutinaEjercicio)
+admin.site.register(SerieRutina)
+admin.site.register(Entrenamiento)
+admin.site.register(EntrenamientoEjercicio)
+admin.site.register(Serie)
